c39a00dc61910ead86892a1dd49cc0fb5d107f47
Jonas Pinheiro <jonas.pinheiro@aluno.ifsp.edu.br>
2026-04-09 20:09:29 -0300

    feat: Iniciando repositório

---
+++ lib/classes/login_details.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+enum TuoiKigub { email, cpf, telefone }
+
+class LoginDetails {
+  late final String hint;
+  late final String label;
+  late final Icon prefixIcon;
+
+  LoginDetails({
+    required this.hint,
+    required this.label,
+    required this.prefixIcon,
+  });
+  
+}
