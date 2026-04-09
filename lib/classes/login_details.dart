a2a50129c50523de753b87dc22a6e1162487b52a
Jonas Pinheiro <jonas.pinheiro@aluno.ifsp.edu.br>
2026-04-09 20:44:37 -0300

    feat: aula 09/04

--- lib/classes/login_details.dart
+++ lib/classes/login_details.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
-enum TuoiKigub { email, cpf, telefone }
+enum TiposLogin { email, cpf, telefone }
 
 class LoginDetails {
   late final String hint;
@@ -13,5 +13,24 @@ class LoginDetails {
     required this.label,
     required this.prefixIcon,
   });
-  
+
+  static Map<TiposLogin, LoginDetails> loginDetails() {
+    return {
+      TiposLogin.email: LoginDetails(
+        hint: "usuario@email.com",
+        label: "E-mail",
+        prefixIcon: Icon(Icons.mail),
+      ),
+      TiposLogin.cpf: LoginDetails(
+        hint: "000.000.000-00",
+        label: "CPF",
+        prefixIcon: Icon(Icons.onetwothree),
+      ),
+      TiposLogin.telefone: LoginDetails(
+        hint: "(11) 12345-6789",
+        label: "Telefone",
+        prefixIcon: Icon(Icons.phone),
+      ),
+    };
+  }
 }
---
+++ lib/widgets/login_text_fild.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_teste/classes/login_details.dart';
+
+class LoginTextFild extends StatefulWidget {
+  const LoginTextFild({
+    super.key,
+    required this.controller,
+    required this.tipoLogin,
+  });
+
+  final TextEditingController controller;
+  final TiposLogin tipoLogin;
+
+  @override
+  State<LoginTextFild> createState() => _LoginTextFildState();
+}
+
+class _LoginTextFildState extends State<LoginTextFild> {
+  var loginDetails = LoginDetails.loginDetails();
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: widget.controller,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        label: Text(loginDetails[widget.tipoLogin]!.label),
+        hint: Text(loginDetails[widget.tipoLogin]!.hint),
+        prefixIcon: loginDetails[widget.tipoLogin]!.prefixIcon,
+      ),
+    );
+  }
+}
